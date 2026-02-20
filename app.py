29e7d748c483763b4eec540d4f7fd035d4493894
Rutvi <140718190+1234rutvi@users.noreply.github.com>
2026-02-20 15:50:04 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = pickle.load(open("model/churn_model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    tenure = float(request.form["tenure"])
+    monthly_charges = float(request.form["monthly_charges"])
+    
+    features = np.array([[tenure, monthly_charges]])
+    prediction = model.predict(features)
+
+    result = "Customer will churn" if prediction[0] == 1 else "Customer will stay"
+
+    return render_template("index.html", prediction_text=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
